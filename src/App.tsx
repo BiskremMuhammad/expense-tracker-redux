d211d9ea5349d66361e9c350c1b11005ce3af9f2
Muhammad Omran <use.yurMind@gmail.com>
2021-04-17 18:02:49 +0200

    add components with dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { AddTransaction } from "./components/add-transaction";
+import { Balance } from "./components/balance";
+import { IncomeExpenses } from "./components/income-expenses";
+import { Transactions } from "./components/transactions";
 import { store } from "./redux/store";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <h1>Hello React Redux</h1>
+      <h2>Expense Tracker</h2>
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <Transactions />
+        <AddTransaction />
       </div>
     </Provider>
   );
