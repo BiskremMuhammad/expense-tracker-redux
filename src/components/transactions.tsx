3e2461be774a61aa03ae8b0b23fc95c886112144
Muhammad Omran <use.yurMind@gmail.com>
2021-04-18 17:12:48 +0200

    transactions history component

--- src/components/transactions.tsx
+++ src/components/transactions.tsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { Dispatch } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Actions, ActionTypes } from "../redux/actions";
+import { GlobalStore, store } from "../redux/store";
+import { Expense } from "../types/types";
 
 export const Transactions = () => {
+  const expenses: Expense[] = useSelector<GlobalStore, Expense[]>(
+    (store: GlobalStore) => store.expenses.expenses
+  );
+  const dispatch = useDispatch<Dispatch<Actions>>();
+
+  const deleteExpense = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    expenseId: string
+  ) => {
+    e.preventDefault();
+    dispatch({
+      type: ActionTypes.DELETE_EXPENSE,
+      payload: expenseId,
+    });
+  };
   return (
     <>
       <h3>History</h3>
       <ul id="list" className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {expenses.map((exp: Expense, i: number) => (
+          <li className={exp.amount > 0 ? "plus" : "minus"}>
+            {exp.description}{" "}
+            <span>
+              {exp.amount < 0 ? "-" : ""}${Math.abs(exp.amount).toFixed(2)}
+            </span>
+            <button
+              className="delete-btn"
+              onClick={(e) => deleteExpense(e, exp.id)}
+            >
+              x
+            </button>
+          </li>
+        ))}
       </ul>
     </>
   );
