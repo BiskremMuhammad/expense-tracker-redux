87810f48b003d9ac7dc06409a9bcc87fcb37796c
Muhammad Omran <use.yurMind@gmail.com>
2021-04-18 16:45:01 +0200

    add type checking to redux store, and add the functionality to balance component

--- src/components/balance.tsx
+++ src/components/balance.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { GlobalStore } from "../redux/store";
+import { Expense, ExpenseStore } from "../types/types";
 
 export const Balance = () => {
+  const expenses: Expense[] = useSelector<GlobalStore, Expense[]>(
+    (store: GlobalStore) => store.expenses.expenses
+  );
+  const balance: number = expenses.reduce<number>((a, b) => a + b.amount, 0);
+
   return (
     <>
       <h4>Your Balance</h4>
-      <h1 id="balance">$0.00</h1>
+      <h1 id="balance">${balance.toFixed(2)}</h1>
     </>
   );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,25 @@
 import { createStore, combineReducers } from "redux";
+import { ExpenseStore } from "../types/types";
 import { expensesReducer } from "./expenese-reducer";
+
+/**
+ * interface that defines the glabal store
+ *
+ * @interface
+ * @exports
+ */
+export interface GlobalStore {
+  /**
+   * the expenses part of the stare
+   *
+   * @type {ExpenseStore}
+   * @memberof GlobalStore
+   */
+  expenses: ExpenseStore;
+}
 
 const reducers = combineReducers({
   expenses: expensesReducer,
 });
 
-export const store = createStore(reducers);
+export const store = createStore<GlobalStore, any, any, any>(reducers);
